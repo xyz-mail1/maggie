a8005d94a9ad9e6ef419c6155e273ab08a944008
xyz-mail1 <sunday.september@protonmail.com>
2024-01-05 09:10:42 +0530

    modify command handler

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -7,12 +7,21 @@ module.exports = (client) => {
     const commandFiles = fs
       .readdirSync(`./src/commands/${folder}`)
       .filter((file) => file.endsWith(".js"));
+
     for (const file of commandFiles) {
       try {
         const command = require(`../commands/${folder}/${file}`);
+
+        // Check if the command name already exists
+        if (client.commands.has(command.name)) {
+          client.logger.warn(`Duplicate command name: ${command.name}`);
+          continue; // Skip loading this command to avoid overwriting
+        }
+
         client.commands.set(command.name, command);
+        client.logger.info(`Loaded command: ${command.name}`);
       } catch (err) {
-        client.logger.error(`Failed to load ${file} Reason: ${err.message}`);
+        client.logger.error(`Failed to load ${file}. Reason: ${err.message}`);
       }
     }
   }
