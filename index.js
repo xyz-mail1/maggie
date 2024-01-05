892fb3c12f2f8dd5f0724f201c4df2f4a8a669fa
xyz-mail1 <sunday.september@protonmail.com>
2024-01-05 18:49:22 +0530

    test

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const token = process.env.token,
   client = new BotClient();
 
 client.loadHandlers();
-
+const { WebhookClient, EmbedBuilder } = require("discord.js");
+const webhook = new WebhookClient({ url: process.env.webhook });
 /*(async () => {
   const test = await client.getColor(
     "https://purrbot.site/img/sfw/cuddle/gif/cuddle_038.gif",
@@ -19,7 +20,30 @@ client.loadHandlers();
   const a = client.getRandomColor();
   console.log(a);
 })();*/
-process.on("unhandledRejection", (err) => console.log(pe.render(err)));
-process.on("uncaughtException", (err) => console.log(pe.render(err)));
+process.on("unhandledRejection", (error) => {
+  if (error.length > 900) {
+    error = error.slice(0, 900) + "...";
+  }
+  if (error.stack) {
+    if (error.stack.length > 900)
+      error.stack = error.stack.slice(0, 900) + "...";
+  }
+  const embed = new EmbedBuilder().addFields([
+    {
+      name: "Error",
+      value: error ? error : "No error",
+    },
+    {
+      name: "Stack",
+      value: error.stack ? error.stack : "No stack",
+    },
+  ]);
+  webhook.send({ embeds: [embed] });
+  console.log(error);
+});
+process.on("uncaughtException", (err) => {
+  console.log("test");
+  console.log(pe.render(err));
+});
 
 client.login(token);
