c1e8f9e54c8369bb365d8a1aab27c33edd5c44e7
xyz-mail1 <sunday.september@protonmail.com>
2024-01-05 08:42:47 +0530

    modify index

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 require("better-module-alias")(__dirname, {
   $purr: "./wrapper/",
 });
-const { gifColor } = require(`./src/helpers/bot`);
+
 const token = process.env.token,
   BotClient = require(`./src/helpers/bot`),
   client = new BotClient();
 
 client.loadHandlers();
-const test = client.gifColor(
+const test = client.getColor(
   "https://purrbot.site/img/sfw/cuddle/gif/cuddle_038.gif",
 );
 const test2 = test.color();
