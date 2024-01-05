e4fbee07739b4d753b7a1c6a7ca51ea87429984e
xyz-mail1 <sunday.september@protonmail.com>
2024-01-05 14:34:57 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ require("dotenv").config();
 require("better-module-alias")(__dirname, {
   $purr: "./wrapper/",
 });
-require("pretty-error").start();
+var PrettyError = require("pretty-error");
+var pe = new PrettyError();
+
 const token = process.env.token,
   BotClient = require(`./src/helpers/bot`),
   client = new BotClient();
 
 client.loadHandlers();
 
-(async () => {
+/*(async () => {
   const test = await client.getColor(
     "https://purrbot.site/img/sfw/cuddle/gif/cuddle_038.gif",
   );
 
   const a = client.getRandomColor();
   console.log(a);
-})();
-process.on("unhandledRejection", (err) =>
-  console.log(`Unhandled exception\n`, err),
-);
+})();*/
+process.on("unhandledRejection", (err) => console.log(pe.render(err)));
 process.on("uncaughtException", (err) =>
-  console.log(`Uncaught Exception\n`, err),
+  console.log(pe.render(`Uncaught Exception\n`, err)),
 );
 
 client.login(token);
